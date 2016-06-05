b641944ae73fb01201317d1b944016988648ee12
chenfei <ideascf@163.com>
2016-06-05 11:25:52 +0800

    Modify.  调整优化职位爬虫，使其能并发执行。
    之前的方式时串行执行的，效率相当低下。
    改进后，爬去时间粗略计算为源时间的1/10

--- lagou/spiders/position.py
+++ lagou/spiders/position.py
@@ -2,6 +2,7 @@
 import json
 import math
 import scrapy
+import functools
 from scrapy.log import logger
 from scrapy.http import Response
 
@@ -15,17 +16,25 @@ class PositionSpider(scrapy.Spider):
     )
 
     position_url = 'http://www.lagou.com/jobs/positionAjax.json?'
-    curpage = 1
     keywords = ['go', 'python', '后端']
-    curkw_index = 0
-    keyword = keywords[0]
-    total_page_num = 0
     JOB_PER_PAGE = 15
 
     def start_requests(self):
-        yield self._gen_form_req()
+        for kw in self.keywords:
+            yield self._gen_form_req(1, kw, callback=functools.partial(self.parse_initial_page, keyword=kw))
 
-    def parse(self, response):
+    def parse_initial_page(self, response, keyword):
+        logger.debug(response.body)
+
+        job_data = json.loads(response.body.decode('utf8'))
+        job_content = job_data['content']
+        job_position_result = job_content['positionResult']
+
+        total_page_num = math.ceil(job_position_result['totalCount'] / self.JOB_PER_PAGE)
+        for pagenum in range(1, total_page_num):
+            yield self._gen_form_req(pagenum, keyword, callback=functools.partial(self.parse_page, keyword=keyword))
+
+    def parse_page(self, response, keyword):
         """
 
         :param response:
@@ -44,7 +53,7 @@ class PositionSpider(scrapy.Spider):
         self.total_page_num = math.ceil(job_position_result['totalCount'] / self.JOB_PER_PAGE)
         for each in job_result:
             item['city'] = each['city']
-            item['keyword'] = self.keyword
+            item['keyword'] = keyword
 
             item['company_size'] = each['companySize']
             item['company_name'] = each['companyName']
@@ -65,18 +74,6 @@ class PositionSpider(scrapy.Spider):
 
             yield item
 
-        # check next
-        if self.curpage < self.total_page_num:
-            self.curpage += 1
-            yield self._gen_form_req()
-        elif self.curkw_index < len(self.keywords)-1:  # 另外的分类
-            self.curpage = 1  # reset page
-            self.total_page_num = 0
-            self.curkw_index += 1  # next kd
-            self.keyword = self.keywords[self.curkw_index]
-
-            yield self._gen_form_req()
-
 
     def _float_salary(self, salary):
         """
@@ -90,15 +87,15 @@ class PositionSpider(scrapy.Spider):
         k_pos = salary.find('k')
         return float(salary[:k_pos])
 
-    def _gen_form_req(self, callback=None):
+    def _gen_form_req(self, pagenum, keyword, callback=None):
         if callback is None:
             callback = self.parse
 
         return scrapy.FormRequest(
             self.position_url,
             formdata={
-                'pn': str(self.curpage),
-                'kd': self.keyword,
+                'pn': str(pagenum),
+                'kd': keyword,
             },
             callback=callback
         )
